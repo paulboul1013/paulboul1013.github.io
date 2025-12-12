74c2ed5b3b1222dbb42b0c95281233d6e3067c05
paulboul1013 <paulboul1013@gmail.com>
2025-12-12 17:21:39 +0800

    add main

---
+++ main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+//foo
+void foo(){
+	puts("foo\n");
+}
+
+void hello(){
+	puts("hello\n");
+}
+
+
+int main(){
+
+
+	hello();
+
+	foo();
+
+
+	return 0;
+}
