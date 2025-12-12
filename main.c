709cbf82d9a74eb621be49b674f88705e2d9fe60
paulboul1013 <paulboul1013@gmail.com>
2025-12-12 17:27:53 +0800

    fix

--- main.c
+++ main.c
@@ -1,22 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-//foo
-void foo(){
-	puts("foo\n");
-}
-
-void hello(){
-	puts("hello\n");
-}
 
 
 int main(){
-
-
-	hello();
-
-	foo();
 
 
 	return 0;
